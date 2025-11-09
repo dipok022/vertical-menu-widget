b434b0ac46a53cb172369752951c89788c328977
dipok <roydipok022@gmail.com>
2025-11-09 20:00:47 +0600

    design 2 upadte

--- js/vertical-menu.js
+++ js/vertical-menu.js
@@ -1,11 +1,26 @@
 // Design 1
-$(document).ready(function ($) {
-  $(".thha-menu-item").on("mouseenter", function () {
-    $(".thha-megamenu-panel").removeClass("active");
+function thhaVerticalMenu(presetClass) {
+  const $preset = $(presetClass);
+
+  // Hover to show mega menu panels
+  $preset.find(".thha-menu-item").on("mouseenter", function () {
+    $preset.find(".thha-megamenu-panel").removeClass("active");
     $(this).find(".thha-megamenu-panel").addClass("active");
   });
 
-  $(".thha-vertical-menu-wrapper").on("mouseleave", function () {
-    $(".thha-megamenu-panel").removeClass("active");
+  //  Remove active when leaving menu area
+  $preset.on("mouseleave", function () {
+    $preset.find(".thha-megamenu-panel").removeClass("active");
   });
+
+  //  Toggle button
+  $preset.find(".thha-vertical-menu-tigger").on("click", function (e) {
+    e.preventDefault();
+    $preset.toggleClass("active-toggle");
+  });
+}
+
+$(document).ready(() => {
+  thhaVerticalMenu(".thha-presets-1");
+  thhaVerticalMenu(".thha-presets-2");
 });
