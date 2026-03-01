33565ff108333b922635cca0278fdd5843180fe6
dipok <roydipok022@gmail.com>
2026-03-02 01:14:57 +0600

    design 3 added

--- 23-01-2026/call-to-action/js/call-to-action.js
+++ 23-01-2026/call-to-action/js/call-to-action.js
@@ -1,1 +1,18 @@
+jQuery(document).ready(function ($) {
+  let $cta = $(".thha-presets-3 .thha-cta-wrapper");
 
+  let observer = new IntersectionObserver(
+    function (entries) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          $cta.addClass("active");
+        }
+      });
+    },
+    { threshold: 0.3 },
+  );
+
+  $cta.each(function () {
+    observer.observe(this);
+  });
+});
