d91ed60016f986697f97fa07b55044f31e2d0370
dipok <roydipok022@gmail.com>
2026-03-02 11:30:42 +0600

    design 4 added in ctc

--- 23-01-2026/call-to-action/js/call-to-action.js
+++ 23-01-2026/call-to-action/js/call-to-action.js
@@ -1,3 +1,5 @@
+// presets 3
+
 jQuery(document).ready(function ($) {
   let $cta = $(".thha-presets-3 .thha-cta-wrapper");
 
@@ -15,4 +17,24 @@ jQuery(document).ready(function ($) {
   $cta.each(function () {
     observer.observe(this);
   });
+});
+
+// presets 4
+jQuery(document).ready(function ($) {
+  function thhaRevealOnScroll() {
+    $(".thha-presets-4 .thha-cta-content, .thha-presets-4 .thha-cta-image")
+      .not(".thha-show")
+      .each(function () {
+        var elementTop = $(this).offset().top;
+        var windowTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        if (windowTop + windowHeight > elementTop + 100) {
+          $(this).addClass("thha-show");
+        }
+      });
+  }
+
+  thhaRevealOnScroll();
+  $(window).on("scroll", thhaRevealOnScroll);
 });
