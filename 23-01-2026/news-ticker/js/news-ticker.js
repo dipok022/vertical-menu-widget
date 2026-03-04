83c56b4bdd258f9bb4a17506ca71c0b4a1786b3d
dipok <roydipok022@gmail.com>
2026-03-04 16:43:57 +0600

    design 2 added

--- 23-01-2026/news-ticker/js/news-ticker.js
+++ 23-01-2026/news-ticker/js/news-ticker.js
@@ -5,9 +5,8 @@ $(document).ready(function () {
   $wrapper.each(function () {
     const $section = $(this);
     const $marquee = $section.find(".thha-marquee");
-    const $trendingWrapper = $section.find(".thha-trending-wrapper");
 
-    $trendingWrapper.hover(
+    $marquee.hover(
       function () {
         $marquee.addClass("thha-paused");
       },
@@ -15,29 +14,105 @@ $(document).ready(function () {
         $marquee.removeClass("thha-paused");
       },
     );
-
-    let isDown = false;
-    let startX;
-    let scrollLeft;
-
-    $trendingWrapper.on("mousedown", function (e) {
-      isDown = true;
-      startX = e.pageX - this.offsetLeft;
-      scrollLeft = this.scrollLeft;
-      $(this).addClass("thha-dragging");
-    });
-
-    $trendingWrapper.on("mouseleave mouseup", function () {
-      isDown = false;
-      $(this).removeClass("thha-dragging");
-    });
-
-    $trendingWrapper.on("mousemove", function (e) {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - this.offsetLeft;
-      const walk = (x - startX) * 2;
-      this.scrollLeft = scrollLeft - walk;
-    });
   });
+});
+
+// presets 2
+$(document).ready(function () {
+  const $wrapper = $(".thha-presets-2");
+  const slidesContainer = $wrapper.find(".thha-slides");
+  const slideHeight = 60;
+  const slideDelay = 4000;
+
+  const firstSlide = slidesContainer.find(".thha-slide").first().clone();
+  const lastSlide = slidesContainer.find(".thha-slide").last().clone();
+
+  slidesContainer.append(firstSlide);
+  slidesContainer.prepend(lastSlide);
+
+  const totalSlides = slidesContainer.find(".thha-slide").length;
+  let currentIndex = 1;
+  let interval;
+  let isAnimating = false;
+
+  slidesContainer.css(
+    "transform",
+    "translateY(-" + currentIndex * slideHeight + "px)",
+  );
+
+  function moveSlide() {
+    slidesContainer.css("transition", ".6s cubic-bezier(.4,0,.2,1)");
+    slidesContainer.css(
+      "transform",
+      "translateY(-" + currentIndex * slideHeight + "px)",
+    );
+  }
+
+  function nextSlide() {
+    if (isAnimating) return;
+    isAnimating = true;
+    currentIndex++;
+    moveSlide();
+
+    setTimeout(() => {
+      if (currentIndex === totalSlides - 1) {
+        slidesContainer.css("transition", "none");
+        currentIndex = 1;
+        slidesContainer.css(
+          "transform",
+          "translateY(-" + currentIndex * slideHeight + "px)",
+        );
+      }
+      isAnimating = false;
+    }, 600);
+  }
+
+  function prevSlide() {
+    if (isAnimating) return;
+    isAnimating = true;
+    currentIndex--;
+    moveSlide();
+
+    setTimeout(() => {
+      if (currentIndex === 0) {
+        slidesContainer.css("transition", "none");
+        currentIndex = totalSlides - 2;
+        slidesContainer.css(
+          "transform",
+          "translateY(-" + currentIndex * slideHeight + "px)",
+        );
+      }
+      isAnimating = false;
+    }, 600);
+  }
+
+  function startAuto() {
+    interval = setInterval(nextSlide, slideDelay);
+  }
+
+  function resetAuto() {
+    clearInterval(interval);
+    startAuto();
+  }
+
+  $wrapper.find("#next").click(function () {
+    nextSlide();
+    resetAuto();
+  });
+
+  $wrapper.find("#prev").click(function () {
+    prevSlide();
+    resetAuto();
+  });
+
+  $wrapper.find(".thha-trending-wrapper").hover(
+    function () {
+      clearInterval(interval);
+    },
+    function () {
+      startAuto();
+    },
+  );
+
+  startAuto();
 });
